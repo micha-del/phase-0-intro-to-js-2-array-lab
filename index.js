66d8658c788125fa2152a08a6ab67a8c40d73dea
Michael Kiros <michael.kiros00@gmail.com>
2022-12-08 20:22:57 -0500

    commit tree dee63c5243dd58a9a19cce13b8615cd41d89c2c2

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat() {
+   cats.push('Ralph')
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(3)
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0)
+}
+
+function appendCat() {
+    const copyOfCats = cats.slice()
+    copyOfCats.push('Broom')
+    return copyOfCats
+}
+
+function prependCat() {
+    const copyOfCats = cats.slice()
+    copyOfCats.unshift('Arnold')
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.pop(2)
+    return copyOfCats
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice()
+    copyOfCats.shift(0)
+    return copyOfCats
+}
